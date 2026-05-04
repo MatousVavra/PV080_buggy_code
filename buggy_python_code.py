01efdf72f7333d21028ac01786dc33e3abb21c2b
Matouš Vávra <132100356+MatousVavra@users.noreply.github.com>
2026-05-04 19:09:31 +0200

    fixes 2

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -4,15 +4,11 @@ import yaml
 import flask
 
 app = flask.Flask(__name__)
-
-
 @app.route("/")
 def index():
     version = flask.request.args.get("urllib_version")
     url = flask.request.args.get("url")
     return fetch_website(version, url)
-
-        
 CONFIG = {"API_KEY": "771df488714111d39138eb60df756e6b"}
 class Person(object):
     def __init__(self, name):
@@ -27,7 +23,6 @@ def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
- 
     try: 
         http = urllib.PoolManager()
         r = http.request('GET', url)
@@ -52,7 +47,7 @@ if __name__ == '__main__':
     print("3. Yaml deserialization vulnerability: see file_solution.yaml for a solution")
     print("4. Use of assert statements vulnerability: run program with -O argument")
     choice  = input("Select vulnerability: ")
-    if choice == "1": 
+    if choice == "1":
         new_person = Person("Vickie")  
         print_nametag(input("Please format your nametag: "), new_person)
     elif choice == "2":
